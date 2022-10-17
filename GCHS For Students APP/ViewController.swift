0e02c826d97ea62f6110906b70edfb42fe8e31de
Pralay Ray <Pray354@d127.org>
2022-10-17 09:39:07 -0500

    Initial Commit

---
+++ GCHS For Students APP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GCHS For Students APP
+//
+//  Created by Pralay T Ray (Student) on 10/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GCHS For Students APPUITests/GCHS_For_Students_APPUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GCHS_For_Students_APPUITestsLaunchTests.swift
+//  GCHS For Students APPUITests
+//
+//  Created by Pralay T Ray (Student) on 10/17/22.
+//
+
+import XCTest
+
+class GCHS_For_Students_APPUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
