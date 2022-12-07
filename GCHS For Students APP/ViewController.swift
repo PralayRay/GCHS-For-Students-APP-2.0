a1937ed1bb7c63dd6e26c112a37e3d3885b20c01
Pralay Ray <Pray354@d127.org>
2022-12-07 09:52:57 -0600

    code

--- GCHS For Students APP/ViewController.swift
+++ GCHS For Students APP/ViewController.swift
@@ -8,11 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var SlideShowViewer: UIImageView!
+    var imageNames = ["1","2","3","4","5","6"]//List of image names
     override func viewDidLoad() {
-        
-      super.viewDidLoad()
-
-   }
-    
+        super.viewDidLoad()
+        let timer = Timer.scheduledTimer(withTimeInterval: 1.5, repeats: true) { timer in
+            self.SlideShowViewer.image = UIImage(named: self.imageNames.randomElement()!) //Slideshow logic
+        }
+        timer.fire() //Starts timer
+        //timer.invalidate() //Stops timer
+    }
 }
